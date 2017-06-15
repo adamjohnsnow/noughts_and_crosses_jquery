bd0263784dcaf088ef839af34f7d5f50df7b3ccf
Adam Snow <adamjohnsnow@icloud.com>
2017-06-15 10:53:17 +0100

    game takes turn

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,9 @@
 function Game(){
   this.board = ['', '', '', '', '', '', '', '', ''];
+  this.turn = 'O'
 }
+
+Game.prototype.takeGo = function (cell) {
+  this.board[cell - 1] = this.turn
+  if (this.turn == 'O') { this.turn = 'X' }
+};
--- tests/gameSpec.js
+++ tests/gameSpec.js
@@ -1,4 +1,4 @@
-describe('game', function(){
+describe('Game', function(){
   var game;
 
   beforeEach(function(done){
@@ -9,4 +9,16 @@ describe('game', function(){
   it('has a game board array', function(){
     expect(game.board).toEqual(['', '', '', '', '', '', '', '', ''])
   })
+
+  it('starts with player O', function(){
+    expect(game.turn).toEqual('O')
+  })
+
+  it('can take a go and switch player', function(done){
+    game.takeGo(5)
+    expect(game.board).toEqual(['', '', '', '', 'O', '', '', '', ''])
+    expect(game.turn).toEqual('X')
+    done();
+  })
+
 })
